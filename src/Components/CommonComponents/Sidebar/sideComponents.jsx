2bfe7a32081c64fd2622387cab84f888bca92118
Anand R P <anand.rp2030@gmail.com>
2023-03-14 19:46:35 +0530

    sidebar design work completed

--- src/Components/CommonComponents/Sidebar/sideComponents.jsx
+++ src/Components/CommonComponents/Sidebar/sideComponents.jsx
@@ -1,12 +1,12 @@
 import { Box, Icon,Flex, Text } from "@chakra-ui/react";
 // import svg from "./logo-svg/homeLogo.svg"
+import style from './sidebar.module.css';
 
 const SideComp = ({icon, name}) => {
-
     return (
-        <Flex>
-            <Icon as={icon} boxSize={6} color='white'/>
-            <Text color='white'> {name}</Text>
+        <Flex className={style.hoverText} h='30px' mt='10px'>
+            <Icon as={icon} boxSize={7} />
+            <Text  ml='10px'> {name}</Text>
         </Flex>
     )
 }
