5eac4ecdefecba83c81a63e618e312cc7358413f
Anand R P <anand.rp2030@gmail.com>
2023-03-14 16:45:10 +0530

    debugger

--- src/Components/CommonComponents/Sidebar/sidebar.jsx
+++ src/Components/CommonComponents/Sidebar/sidebar.jsx
@@ -1,31 +1,25 @@
-import { Box , Icon} from "@chakra-ui/react";
+import { Box, Icon } from "@chakra-ui/react";
 import SideComp from "./sideComponents";
-import { HiOutlineHome } from 'react-icons/hi'
-import { IoSearchOutline } from 'react-icons/io5'
-import { BiLibrary } from 'react-icons/bi'
-import { BsPlusSquareFill } from 'react-icons/bs'
-
-
-
+import { HiOutlineHome } from "react-icons/hi";
+import { IoSearchOutline } from "react-icons/io5";
+import { BiLibrary } from "react-icons/bi";
+import { BsPlusSquareFill } from "react-icons/bs";
+import SpotifyIcon from "./logo-svg/LikedSong";
 
 const Sidebar = () => {
-    
-
   return (
     <div>
       <Box bgColor="#000" h="680px" w="240px" pl="23px" pt="23px">
         <img
           src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
           alt="logo"
-          style={{maxWidth:"130px"}}
+          style={{ maxWidth: "130px" }}
         />
 
-        
-        <SideComp icon={HiOutlineHome} name="Home"/>
-        <SideComp icon={IoSearchOutline} name="Search"/>
-        <SideComp icon={BiLibrary} name="Library"/>
-          
-          
+        <SpotifyIcon />
+        <SideComp icon={HiOutlineHome} name="Home" />
+        <SideComp icon={IoSearchOutline} name="Search" />
+        <SideComp icon={BiLibrary} name="Library" />
       </Box>
     </div>
   );
