1b06148134270b1462b31050366cf65dfd57853f
Suraj <surajbloodstone@gmail.com>
2023-03-16 03:29:39 +0530

    added footer

---
+++ src/Components/Upgrade/UpgradeComponents/FooterIcon.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterIcon = () => {
+	return <div></div>;
+};
+
+export default FooterIcon;
--- src/Components/Upgrade/UpgradeComponents/PlansData.jsx
+++ src/Components/Upgrade/UpgradeComponents/PlansData.jsx
@@ -4,7 +4,6 @@ import { CheckIcon } from "@chakra-ui/icons";
 import { motion } from "framer-motion";
 
 const PlansData = ({ ele }) => {
-	console.log(ele);
 	return (
 		<Flex
 			bg="white"
