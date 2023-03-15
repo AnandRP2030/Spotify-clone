885e78f13407dbf5fba98962f6e0475f0ca2f575
Anand R P <anand.rp2030@gmail.com>
2023-03-15 23:55:40 +0530

    like page header finished

--- src/Components/Like/like.jsx
+++ src/Components/Like/like.jsx
@@ -1,12 +1,15 @@
 import Sidebar from "../CommonComponents/Sidebar/sidebar";
-
+import LikeHeader from "./LikeComponents/LikeHeader";
+import Navbar from "../CommonComponents/Navbar/Navbar";
 
 const Like = () => {
-    return (
-        <div>
-            <Sidebar/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Sidebar />
+      <Navbar/>
+      <LikeHeader />
+    </div>
+  );
+};
 
-export {Like};
+export { Like };
