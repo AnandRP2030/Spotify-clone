ba880a4ca1c2ee8e9e7f203271d69dd88228506e
Suraj <surajbloodstone@gmail.com>
2023-03-16 03:38:45 +0530

    added social icons

--- src/Components/Upgrade/UpgradeComponents/FooterIcon.jsx
+++ src/Components/Upgrade/UpgradeComponents/FooterIcon.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { Icon, Flex } from "@chakra-ui/react";
 const FooterIcon = () => {
-	return <div></div>;
+	return (
+		<Flex>
+			<Icon as={BsTwitter} />
+			<Icon as={BsInstagram} />
+			<Icon as={FaFacebookF} />
+		</Flex>
+	);
 };
 
 export default FooterIcon;
