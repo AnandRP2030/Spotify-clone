17e3749469cc62c566f76e308d5311f024a7714d
Anand R P <anand.rp2030@gmail.com>
2023-03-16 12:51:39 +0530

    sidebar linked with pages

--- src/Components/CommonComponents/Sidebar/sidebar.jsx
+++ src/Components/CommonComponents/Sidebar/sidebar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Box, Text, Flex, Grid, Icon } from "@chakra-ui/react";
 import SideComp from "./sideComponents";
 import { HiOutlineHome } from "react-icons/hi";
@@ -11,14 +12,13 @@ const Sidebar = () => {
     <Box
       className={style.sideBox}
       bg="#000000"
-      position={'fixed'}
+      position={"fixed"}
       w="196px"
-     p={'24px'}
-      // zIndex={5}
-      top='0'
-      bottom={'0'}
-     left='0'
-
+      p={"24px"}
+      zIndex={5}
+      top="0"
+      bottom={"0"}
+      left="0"
     >
       <img
         src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
@@ -26,14 +26,16 @@ const Sidebar = () => {
         style={{ maxWidth: "130px", cursor: "pointer" }}
       />
 
-      <Box mt="12px" p={'10px 0'} color='#b3b3b3' >
-        <SideComp icon={HiOutlineHome} name="Home"  />
+      <Box mt="12px" p={"10px 0"} color="#b3b3b3">
+        <Link to="/">
+          <SideComp icon={HiOutlineHome} name="Home" />
+        </Link>
         <SideComp icon={IoSearchOutline} name="Search" />
         <SideComp icon={BiLibrary} name="Library" />
       </Box>
 
-      <Grid h='150px'  mt='20px'  borderBottomColor='white'>
-        <Flex mt='20px' className={style.hoverText}>
+      <Grid h="150px" mt="20px" borderBottomColor="white">
+        <Flex mt="20px" className={style.hoverText}>
           <img
             width="25px"
             style={{ height: "25px" }}
@@ -51,10 +53,12 @@ const Sidebar = () => {
             src="https://raw.githubusercontent.com/AnandRP2030/Resource-website/master/images/love.jpg"
             alt="icon"
           />
-          <Text ml="13px" color="#b3b3b3">
-            {" "}
-            Liked Songs
-          </Text>
+          <Link to="/like">
+            <Text ml="13px" color="#b3b3b3">
+              {" "}
+              Liked Songs
+            </Text>
+          </Link>
         </Flex>
         <Flex mt="20px" className={style.hoverText}>
           <img
@@ -70,13 +74,13 @@ const Sidebar = () => {
         </Flex>
       </Grid>
 
-      <Box  h="180px" w="100%" mt="20px">
+      <Box h="180px" w="100%" mt="20px">
         {/* playlist box work pending  */}
       </Box>
-        
-      <Flex style={{cursor:'pointer'}} color='white' w="100%" h="50px"  >
-        <Icon as={FiDownload} boxSize={6}  />
-        <Text ml='15px'> Install App </Text>
+
+      <Flex style={{ cursor: "pointer" }} color="white" w="100%" h="50px">
+        <Icon as={FiDownload} boxSize={6} />
+        <Text ml="15px"> Install App </Text>
       </Flex>
     </Box>
   );
