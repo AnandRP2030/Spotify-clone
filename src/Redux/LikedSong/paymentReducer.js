9457b5028d1f6fed9ea2e248cd7b2e889e261fd0
Suraj <surajbloodstone@gmail.com>
2023-03-16 23:16:27 +0530

    added reducer

---
+++ src/Redux/LikedSong/paymentReducer.js
@@ -0,0 +1,47 @@
+const initialState={plans:[
+    {
+        free: false,
+        plan: "Mini",
+        day: "From ₹7/day",
+        account: "1 account on mobile only",
+        adFree: "Ad-free music listening on mobile",
+        group: "Group Session",
+        download: "Download 30 songs on 1 mobile device",
+    },
+    {
+        free: true,
+        plan: "Individual",
+        day: "₹119/month after offer period",
+        account: "1 account",
+        adFree: "Ad-free music listening",
+        group: "Group Session",
+        download: "Download 10k songs/device on 5 devices",
+    },
+    {
+        free: true,
+        plan: "Duo",
+        day: "₹149/month after offer period",
+        family: "For couples who live together",
+        account: "2 accounts",
+        adFree: "Ad-free music listening",
+        group: "Group Session",
+        download: "Download 10k songs/device, on 5 devices per account",
+    },
+    {
+        free: true,
+        plan: "Family",
+        day: "₹179/month after offer period",
+        family: "For family who live together",
+        account: "Up to 6 accounts",
+        adFree: "Ad-free music listening on mobile",
+        group: "Group Session",
+        download: "Download 30 songs on 1 mobile device",
+    },
+],
+}
+
+const PaymentReducer = (state=initialState, action)=>{
+ return state;
+
+}
+export {PaymentReducer}
