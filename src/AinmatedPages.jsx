586221a02b8b7055f0efca0f0375490d1753acb0
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-03-17 01:06:34 +0530

    home-page-animation-in-precess

---
+++ src/AinmatedPages.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+const animations = {
+  initial: { opacity: 0, x: 100 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
+const AnimatedPages = ({ children }) => {
+  return (
+    <motion.div
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 2 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+export default AnimatedPages;
