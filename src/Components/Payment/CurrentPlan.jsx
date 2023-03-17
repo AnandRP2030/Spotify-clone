55aa45d85cb0a232b36346f3d640ee1aed60b64a
Suraj <surajbloodstone@gmail.com>
2023-03-18 00:11:51 +0530

    updated

---
+++ src/Components/Payment/CurrentPlan.jsx
@@ -0,0 +1,21 @@
+import { Box, Button, Center, Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const CurrentPlan = ({month, price, plan,bg,color}) => {
+  return (
+    <Box p={3} mx={4}  color={color} mt="10px">
+        <Box bg={bg} p={4} borderTopLeftRadius="10px" borderTopRightRadius="10px">
+            <Text fontSize='20px'>Premium {plan}</Text>
+            <Text fontSize="12px">1 Premium Account</Text>
+        </Box>
+        <Flex bg="white" color="black" p={4} borderBottomLeftRadius="10px" borderBottomRightRadius="10px">
+            <Text flex="1" >Starting Today</Text>
+            <Text flex="2" align="right" >One time payment of {price}.00 INR for {month} months</Text>
+        </Flex>
+        <Center mt="10px">
+
+        <Button colorScheme="green" size="lg">Continue Payment</Button>
+        </Center>
+    </Box>
+  )
+}
