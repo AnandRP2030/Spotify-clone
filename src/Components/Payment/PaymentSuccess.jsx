6f77dbf5617c926e9a8b866e3658aa4882268ffc
Suraj <surajbloodstone@gmail.com>
2023-03-18 18:20:39 +0530

    updated payment success

--- src/Components/Payment/PaymentSuccess.jsx
+++ src/Components/Payment/PaymentSuccess.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Box, Center, Image, Text } from '@chakra-ui/react'
+import Navbar from '../Upgrade/UpgradeComponents/Navbar'
 
 export const PaymentSuccess = () => {
     const order = Math.random()
   return (
    
     <Box >
-    <Image src="https://cdn.dribbble.com/users/1751799/screenshots/5512482/check02.gif" mx="auto" />
-    <Center mt={-12}>
+        <Navbar />
+    <Image src="https://cdn.dribbble.com/users/1751799/screenshots/5512482/check02.gif" mx="auto"/>
+    <Center mt="-60px">
          <Text color="#00d34f" fontSize="56px" fontWeight="bold">You're Premium now.</Text>
          
     </Center>
