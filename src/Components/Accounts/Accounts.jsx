8f84718d7ccf78922682087451de91d11249e4d7
Anand R P <anand.rp2030@gmail.com>
2023-03-20 15:02:24 +0530

    profile page completed

--- src/Components/Accounts/Accounts.jsx
+++ src/Components/Accounts/Accounts.jsx
@@ -14,7 +14,7 @@ import {
   Button,
   Circle
 } from "@chakra-ui/react";
-
+import ProfileComp from "../CommonComponents/Profile/ProfileComp";
 const Account = () => {
   const useDetails = JSON.parse(localStorage.getItem("userDetail")) || [
     { given_name: "Username" },
@@ -29,15 +29,19 @@ const Account = () => {
     <Box>
       <Navbar />
 
-      <Box w="100%" h="1500px">
+      <Box w="100%" h="1300px">
         <Image
           w="100%"
           src="https://raw.githubusercontent.com/AnandRP2030/git-learn/master/premium.png"
           alt="img"
         />
-        <Box p="100px" w="80%" zIndex={-10} pos="relative" left="20%" h="800px">
+
+          <ProfileComp/>
+
+        <Box p="100px" w="80%" zIndex={-10} pos="relative" bottom='853px' left="22%" h="500px">
+          
           <Box>
-            <Text fontSize="5xl" as="b" mb="50px">
+            <Text fontSize="5xl" as="b" >
               {" "}
               Account Overview
             </Text>
@@ -47,11 +51,12 @@ const Account = () => {
               Profile
             </Text>
 
-            <Circle pos='relative'  size="100px" bg="black" left='48%' bottom='100px' color="white">
+            {/* <Circle pos='relative'  size="100px" bg="black" left='48%' bottom='100px' color="white">
               <Image src={useDetails[0].picture} borderRadius="50%" alt="Proile Picuture"/>
-            </Circle>
+            </Circle> */}
 
-            <TableContainer pos='relative' right='25px'>
+
+            <TableContainer pos='relative' right='25px' >
               <Table variant="simple">
                 <Tbody>
                   <Tr fontSize="20px">
@@ -75,15 +80,17 @@ const Account = () => {
             </TableContainer>
             <Button
               style={{ cursor: "pointer" }}
-              mt="80px"
+            
               bg="black"
               color="white"
+              pos='relative'
+              top='20px'
             >
               {" "}
               Edit Profile
             </Button>
           </Box>
-          <Box mt="50px">
+          <Box mt="120px">
             <Text fontSize="3xl" as="b" mt="10px">
               {" "}
               Sign out everywhere{" "}
@@ -97,7 +104,7 @@ const Account = () => {
               and TVs), so for those go to your apps page and choose REMOVE
               ACCESS.
             </Text>
-            <Button mt="10px"  variant="outline">
+            <Button mt='10px' variant="outline" color='white' bg='black' >
               {" "}
               Sign out everywhere
             </Button>
