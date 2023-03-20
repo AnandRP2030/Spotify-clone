f6c674b353d51bd07441ab6a49a257fcb42b9e82
Suraj <surajbloodstone@gmail.com>
2023-03-20 14:12:00 +0530

    added premium auth to lyrics part

--- src/Components/Login/SignUp/SignupRedux/SignupReducer.js
+++ src/Components/Login/SignUp/SignupRedux/SignupReducer.js
@@ -4,37 +4,42 @@ const initialState = {
   UserData: [],
   // UserDataByForm: [],
   error: false,
-  isPremiumUser:false
-  };
-  
-  const SignupReducer = (state = initialState,action) => {
-    switch(action.type){
-        case "LOGIN_SUCCESS":{
-          return{
-            ...state,
-            token:action.payload.sub,
-            isAuth:true,
-            UserData:[action.payload],
-          }
-      
-        }
-        // case "LOGIN_SUCCESS_BY_FORM":{
-        //   return{
-        //     ...state,
-        //     isAuth:true,
-        //     UserDataByForm:[action.payload],
-        //   }
-      
-        // }
-        case "LOGOUT_USER":{
-          return{
-            ...state,
-            isAuth:false,
-          }
-        }
-        default:
-        return state;
-      }
-  };
-  
-  export { SignupReducer };
+  isPremiumUser: false,
+};
+
+const SignupReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOGIN_SUCCESS": {
+      return {
+        ...state,
+        token: action.payload.sub,
+        isAuth: true,
+        UserData: [action.payload],
+      };
+    }
+    // case "LOGIN_SUCCESS_BY_FORM":{
+    //   return{
+    //     ...state,
+    //     isAuth:true,
+    //     UserDataByForm:[action.payload],
+    //   }
+
+    // }
+    case "LOGOUT_USER": {
+      return {
+        ...state,
+        isAuth: false,
+      };
+    }
+    case "PREMIUM USER": {
+      return {
+        ...state,
+        isPremiumUser: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export { SignupReducer };
